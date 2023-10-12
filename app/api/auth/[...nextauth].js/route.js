61b266fbb4f1044ee5c0a18240f64fb562b73e38
Titouan Hirsch <titouanhirsch@gmail.com>
2023-10-12 21:36:13 +0200

    comment next uath file

--- app/api/auth/[...nextauth].js/route.js
+++ app/api/auth/[...nextauth].js/route.js
@@ -1,22 +1,22 @@
-import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-import prisma from "@/lib/prisma"
+// import NextAuth from "next-auth"
+// import CredentialsProvider from "next-auth/providers/credentials"
+// import { PrismaAdapter } from "@auth/prisma-adapter"
+// import { PrismaClient } from "@prisma/client"
+// import prisma from "@/lib/prisma"
 
-export default NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    CredentialsProvider({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
-      name: 'Credentials',
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        username: { label: "Numéro de Téléphone", type: "tel", placeholder: "06 08 72 58 71" },
-        password: { label: "Password", type: "password" }
-      },
-  ]
-})
+// export default NextAuth({
+//   adapter: PrismaAdapter(prisma),
+//   providers: [
+//     CredentialsProvider({
+//       // The name to display on the sign in form (e.g. 'Sign in with...')
+//       name: 'Credentials',
+//       // The credentials is used to generate a suitable form on the sign in page.
+//       // You can specify whatever fields you are expecting to be submitted.
+//       // e.g. domain, username, password, 2FA token, etc.
+//       // You can pass any HTML attribute to the <input> tag through the object.
+//       credentials: {
+//         username: { label: "Numéro de Téléphone", type: "tel", placeholder: "06 08 72 58 71" },
+//         password: { label: "Password", type: "password" }
+//       },
+//   ]
+// })
