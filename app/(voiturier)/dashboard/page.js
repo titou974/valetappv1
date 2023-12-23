cf35522a67efe1a036d0d2dcca7210c1cf3c6761
Titouan Hirsch <titouanhirsch@gmail.com>
2023-12-23 22:57:45 +0100

    fixnavbarheader

--- app/(voiturier)/dashboard/page.js
+++ app/(voiturier)/dashboard/page.js
@@ -8,7 +8,8 @@ import TimeCounter from '@/app/components/timecounter';
 import StartingHour from '@/app/components/startinghour';
 import { PlayCircleIcon } from '@heroicons/react/20/solid';
 import style from "../../startbutton.module.css";
-import DashboardLogged from '@/app/components/dashboard';
+import DashboardLogged from '@/app/components/dashboardlogged';
+import DashboardNotLogged from '@/app/components/dashboardnotlogged';
 
 
 const Dashboard = async () => {
@@ -22,54 +23,12 @@ const Dashboard = async () => {
 
 
   if (!session) {
-    return (
-        <div className='bg-black h-screen w-full text-white'>
-          <div className={`${styles.padding} flex flex-col justify-between h-full`}>
-            <div>
-              <h2 className={`${styles.subText}`}>Pour débuter,</h2>
-              <h2 className={`${styles.headText}`}>Connectez-vous</h2>
-            </div>
-            <div>
-                <Link href="/sign-in">
-                  <button className="bg-primary w-full py-3 rounded-full flex items-center justify-center gap-2 hover:bg-white transition-colors mb-10">
-                    <p className="text-black font-semibold text-[32px]">Se Connecter</p>
-                    <div className="w-[26px] text-black">
-                      <svg fill="none" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                      </svg>
-                    </div>
-                  </button>
-                </Link>
-            </div>
-            <div className="text-center">
-              <p className="text-white">Nestor App 🇫🇷</p>
-            </div>
-          </div>
-        </div>
-      )
+    return <DashboardNotLogged />
   } else {
     console.log(session);
     const startingHour = new Date(session.user.startingHourSession)
     console.log(startingHour);
-    return (
-      <div className='bg-black h-full w-full text-white'>
-        <div className={`${styles.padding} flex flex-col justify-center gap-20 h-full`}>
-          <div className={`fixed ${styles.padding} top-0 left-0`}>
-            <h2 className={`${styles.subText}`}>Bon courage,</h2>
-            <h2 className={`${styles.headText}`}>{session.user.name} 🚗</h2>
-          </div>
-          <div className='emptyDiv'>
-
-          </div>
-          <div>
-            <DashboardLogged siteName={session.user.siteName} sessionId={session.user.sessionId} />
-            {/* <div className="text-center fixed bottom-1 left-1 right-1">
-              <p className="text-white">Nestor App 🇫🇷</p>
-            </div> */}
-          </div>
-        </div>
-      </div>
-    )
+    return <DashboardLogged siteName={session.user.siteName} sessionId={session.user.sessionId} userName={session.user.name} />
   }
 }
 
